d2c51233156d006194499a54062cab540d137ef7
Juan Ramírez <jramirez@plainconcepts.com>
2022-06-14 19:37:36 +0200

    add secrets 2

--- src/CapacityPlanner/Controllers/WeatherForecastController.cs
+++ src/CapacityPlanner/Controllers/WeatherForecastController.cs
@@ -17,25 +17,7 @@ namespace CapacityPlanner.Controllers
         }
 
 
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
-
-
-        [HttpGet(Name = "GetSecrets")]
+        [HttpGet(Name = "GetSecrets")]        
         public async Task<IEnumerable<string>> GetSecrets()
         {
 
