cb3f54132a02c2e01ce65423a1f8b03f7fb5d908
Juan Ramírez <jramirez@plainconcepts.com>
2022-06-22 20:59:34 +0200

    redefine pipelines and query

--- src/HotelCatalog/Services/HotelCatalogService.cs
+++ src/HotelCatalog/Services/HotelCatalogService.cs
@@ -1,5 +1,4 @@
 ﻿using Dapr.Client;
-using System.Linq;
 using HotelCatalog.Models;
 
 namespace HotelCatalog.Services
@@ -7,10 +6,14 @@ namespace HotelCatalog.Services
     public class HotelCatalogService
     {
         private readonly DaprClient _daprClient;
+        private readonly Logger<HotelCatalogService> _logger;
+
+
         private string STORE_NAME = "cosmosdb-state"; //"redis-state"; //"blobstorage-state";
-        public HotelCatalogService(DaprClient daprClient)
+        public HotelCatalogService(DaprClient daprClient, Logger<HotelCatalogService> logger)
         {
             _daprClient = daprClient;
+            _logger = logger;
         }
 
         public Task<Hotel> GetHotel(string code, CancellationToken cancellationToken)
@@ -20,10 +23,25 @@ namespace HotelCatalog.Services
 
         public Task SaveOrUpdateHotel(Hotel hotel, CancellationToken cancellationToken)
         {
-             return _daprClient.SaveStateAsync(STORE_NAME, hotel.Code, hotel, cancellationToken: cancellationToken);
+            return _daprClient.SaveStateAsync(STORE_NAME, hotel.Code, hotel, cancellationToken: cancellationToken);
         }
 
         public async Task<IEnumerable<Hotel>> GetHotels(string countryCode, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await GetHotelsWithQuery(countryCode, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error get catalog from state with query: {ex.Message}, retry without it....");
+
+                return await GetHotelsWithFilter(countryCode, cancellationToken); throw;
+            }
+        }
+
+
+        private async Task<IEnumerable<Hotel>> GetHotelsWithQuery(string countryCode, CancellationToken cancellationToken)
         {
             var jsonQuery = @$"
             {{
@@ -37,8 +55,15 @@ namespace HotelCatalog.Services
                 ["queryIndexName"] = "countryIndex"
             };
 
-            var queryResponse =  await _daprClient.QueryStateAsync<Hotel>(STORE_NAME, jsonQuery, metaproperties, cancellationToken: cancellationToken);
+            var queryResponse = await _daprClient.QueryStateAsync<Hotel>(STORE_NAME, jsonQuery, metaproperties, cancellationToken: cancellationToken);
             return queryResponse.Results.Select(x => x.Data);
+        }
+
+        private async Task<IEnumerable<Hotel>> GetHotelsWithFilter(string countryCode, CancellationToken cancellationToken)
+        {
+            var response = await _daprClient.GetStateAsync<IEnumerable<Hotel>>(STORE_NAME, "hotels", cancellationToken: cancellationToken);
+
+            return response.Where(h => h.CountryCode == countryCode);
         }
     }
 }
